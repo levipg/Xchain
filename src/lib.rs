1cddabadb9c9c7468b6c50c2cc420489721c1761
Baji <68044099+youtubaijia@users.noreply.github.com>
2017-02-03 13:08:58 +0800

    update the network protocol tokio thingy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 extern crate xblockchain;
 extern crate xblockchain_storage;
 extern crate exe_common;
+extern crate protocol_tokio as protocol;
+extern crate futures;
+extern crate tokio;
 
 pub mod gclock;
 pub mod blockchain;
