aaf9514c48a1426ff3c8f9421e433881a3a2788f
Baji <68044099+youtubaijia@users.noreply.github.com>
2017-03-02 04:52:16 +0800

    move blockchain to own directory

---
+++ src/blockchain/mod.rs
@@ -0,0 +1,27 @@
+use xblockchain_storage::StorageConfig;
+use xblockchain_storage::Storage;
+
+/// Contains a locally known fork of the blockchain
+pub struct BlockchainFork {
+}
+
+#[allow(dead_code)]
+pub struct Blockchain {
+    /// the storage for the overall blockchains (blocks)
+    storage: Storage,
+    /// possible other known forks
+    heads: Vec<BlockchainFork>,
+    /// what we think is the real blockchain at this specific moment
+    tip: BlockchainFork,
+}
+
+impl Blockchain {
+    pub fn from_storage(storage_config: &StorageConfig) -> Self {
+        let storage = Storage::init(storage_config).unwrap();
+        Blockchain {
+            storage: storage,
+            heads: Vec::new(),
+            tip: BlockchainFork {},
+        }
+    }
+}
