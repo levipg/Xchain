ff768a9360434f0901ab78b31e8cefead13929bb
Baji <68044099+youtubaijia@users.noreply.github.com>
2017-03-14 09:25:02 +0800

    fast commit

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,11 @@ extern crate futures;
 extern crate tokio;
 #[macro_use]
 extern crate structopt;
-#[macro_use]
-extern crate serde_derive;
 
 pub mod clock;
 pub mod blockchain;
 pub mod tpool;
 pub mod state;
-pub mod network;
 pub mod utils;
 pub mod intercom;
 pub mod settings;
