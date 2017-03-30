0ad983d4274c2b6b3f5082f6b0a06b7e574b9814
Baji <68044099+youtubaijia@users.noreply.github.com>
2017-03-30 14:35:42 +0800

    use the right configuration for the listener

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -40,7 +40,7 @@ pub fn run( config: network::Configuration
         channels: channels,
     };
 
-    let peer_iter = stream::iter_ok(config.peer_nodes).for_each(move |peer| {
+    let peer_iter = stream::iter_ok(config.listen_to).for_each(move |peer| {
         match peer.connection {
             network::Connection::Socket(sockaddr) => {
                 run_listen_socket(sockaddr, peer, state.clone())
@@ -53,50 +53,57 @@ pub fn run( config: network::Configuration
     tokio::run(peer_iter);
 }
 
-fn run_listen_socket(sockaddr: SocketAddr, peer: Peer, state: State)
+fn run_listen_socket(sockaddr: SocketAddr, peer: Listen, state: State)
     -> tokio::executor::Spawn
 {
     let server = TcpListener::bind(&sockaddr).unwrap().incoming()
         .map_err(move |err| {
-            error!("Error while accepting connection from {:?}", sockaddr)
+            error!("Error while accepting connection from {:?}: {:?}", sockaddr, err)
         }).for_each(move |stream| {
+            let state = state.clone();
             Connection::accept(stream)
-                .map_err(move |err| error!("Rejecting NTT connection from {:?}", sockaddr))
-                .and_then(|connection| {
-                    let (sink, stream) = connection.split();
-
-                    let (sink_tx, sink_rx) = mpsc::unbounded();
-
-                    let stream = stream.for_each(move |inbound| {
-                        match inbound {
-                            Inbound::NewNode(lwcid, node_id) => {
-                                sink_tx.unbounded_send(Message::AckNodeId(lwcid, node_id)).unwrap();
-                            },
-                            inbound => {
-                                println!("inbound: {:?}", inbound);
-                            }
-                        }
-                        future::ok(())
-                    }).map_err(|err| {
-                        println!("connection stream error {:#?}", err)
-                    });
-
-                    let sink = sink_rx.fold(sink, |sink, outbound| {
-                        match outbound {
-                            Message::AckNodeId(_lwcid, node_id) => {
-                                future::Either::A(sink.ack_node_id(node_id)
-                                    .map_err(|err| println!("err {:?}", err)))
-                            },
-                            message => future::Either::B(sink.send(message)
-                                    .map_err(|err| println!("err {:?}", err)))
-                        }
-                    }).map(|_| ());
-
-                    let connection_task = stream.select(sink)
-                        .then(|_| { println!("closing connection"); Ok(()) });
-
-                    tokio::spawn(connection_task)
+                .map_err(move |err| error!("Rejecting NTT connection from {:?}: {:?}", sockaddr, err))
+                .and_then(move |connection| {
+                    let state = state.clone();
+                    tokio::spawn(run_connection(state, connection))
                 })
         });
     tokio::spawn(server)
+}
+
+fn run_connection<T>(state: State, connection: Connection<T>)
+    -> impl future::Future<Item = (), Error = ()>
+  where T: tokio::io::AsyncRead + tokio::io::AsyncWrite
+{
+    let (sink, stream) = connection.split();
+
+    let (sink_tx, sink_rx) = mpsc::unbounded();
+
+    let stream = stream.for_each(move |inbound| {
+        match inbound {
+            Inbound::NewNode(lwcid, node_id) => {
+                sink_tx.unbounded_send(Message::AckNodeId(lwcid, node_id)).unwrap();
+            },
+            inbound => {
+                println!("inbound: {:?}", inbound);
+            }
+        }
+        future::ok(())
+    }).map_err(|err| {
+        println!("connection stream error {:#?}", err)
+    });
+
+    let sink = sink_rx.fold(sink, |sink, outbound| {
+        match outbound {
+            Message::AckNodeId(_lwcid, node_id) => {
+                future::Either::A(sink.ack_node_id(node_id)
+                    .map_err(|err| println!("err {:?}", err)))
+            },
+            message => future::Either::B(sink.send(message)
+                    .map_err(|err| println!("err {:?}", err)))
+        }
+    }).map(|_| ());
+
+    stream.select(sink)
+        .then(|_| { println!("closing connection"); Ok(()) })
 }
