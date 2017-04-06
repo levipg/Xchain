10d04a64e8e2eae8ca3cbb9b2d3ea244d505e009
Baji <68044099+youtubaijia@users.noreply.github.com>
2017-04-06 19:32:45 +0800

    remove usage of println

---
+++ src/blockcfg.rs
@@ -0,0 +1,7 @@
+use xblockchain;
+
+pub type GenesisData = xblockchain::config::GenesisData;
+pub type TransactionId = xblockchain::tx::TxId;
+pub type Transaction = xblockchain::tx::TxAux;
+pub type BlockHash = xblockchain::block::HeaderHash;
+pub type Block = xblockchain::block::Block;
