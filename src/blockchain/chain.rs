c3185524026fcc9f514b5fe58efdd60f36128d42
Baji <68044099+youtubaijia@users.noreply.github.com>
2017-07-04 05:13:33 +0800

    use an appropriate intermediate type for the NetworkBloadcastMsg

--- src/blockchain/chain.rs
+++ src/blockchain/chain.rs
@@ -1,7 +1,7 @@
 use std::sync::{Arc, RwLock};
 
 use xblockchain_storage::StorageConfig;
-use xblockchain_storage::{tag, Storage};
+use xblockchain_storage::Storage;
 
 use super::chain_types::ChainTips;
 use super::super::blockcfg::{GenesisData, BlockHash};
@@ -14,33 +14,24 @@ pub struct Blockchain {
     /// possible other known forks
     heads: ChainTips<BlockHash>,
     /// what we think is the real blockchain at this specific moment
-    tip: BlockHash,
+    tip: Option<BlockHash>,
 }
 
 pub type BlockchainR = Arc<RwLock<Blockchain>>;
 
-// FIXME: copied from xblockchain-cli
-pub const LOCAL_BLOCKCHAIN_TIP_TAG : &'static str = "tip";
-
 impl Blockchain {
     pub fn from_storage(genesis_data: &GenesisData, storage_config: &StorageConfig) -> Self {
         let storage = Storage::init(storage_config).unwrap();
-        let genesis_hash = genesis_data.genesis_prev.clone();
-        let tip = tag::read_hash(&storage, &LOCAL_BLOCKCHAIN_TIP_TAG).unwrap_or(genesis_hash.clone());
         Blockchain {
-            genesis_hash,
+            genesis_hash: genesis_data.genesis_prev.clone(),
             storage: storage,
             heads: ChainTips::new(),
-            tip,
+            tip: None,
         }
     }
 
     /// return the latest
     pub fn get_tip(&self) -> BlockHash {
-        self.tip.clone()
-    }
-
-    pub fn get_storage(&self) -> &Storage {
-        &self.storage
+        self.genesis_hash.clone()
     }
 }
